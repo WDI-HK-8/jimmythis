ae4a2d638e494fb5f45ce3c0b2967e4220963c1e
Yohan <yohanology@gmail.com>
2015-08-24 21:19:06 +0800

    ft(models_relations): Add relations service/rating

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,4 +1,5 @@
 class Service < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
+  has_many :ratings
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150824123232) do
+ActiveRecord::Schema.define(version: 20150824124155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20150824123232) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "ratings", force: :cascade do |t|
+    t.text     "comment"
+    t.integer  "grade"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "service_id"
   end
 
   create_table "services", force: :cascade do |t|
