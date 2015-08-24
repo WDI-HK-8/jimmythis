cc7aef371c8fbfd57c2ec09955e0c7a75940f12d
Carson Trent Long <CarsonLong@IOS-Syncy.local>
2015-08-24 15:39:04 +0800

    ft(login): initialized the login page

--- db/schema.rb
+++ db/schema.rb
@@ -41,4 +41,5 @@ ActiveRecord::Schema.define(version: 20150824061817) do
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true, using: :btree
+
 end
