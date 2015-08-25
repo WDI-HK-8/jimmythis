646602cc806b8a80d8b67b0f310d322226a8b52d
Carson Trent Long <CarsonLong@IOS-Syncy.local>
2015-08-25 09:44:51 +0800

    ft(development): added phone and description to action controller for signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,8 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:sign_up) << :phone
+    devise_parameter_sanitizer.for(:sign_up) << :description
   end
 
 end
