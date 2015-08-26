0551c3107f797b57124cc079c3e435b5290861fc
Carson Trent Long <CarsonLong@IOS-Syncy.local>
2015-08-26 13:40:28 +0800

    updated ratings system

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,10 +2,14 @@ class RatingsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @rating = Service.find(params[:service_id]).ratings.create(rating_params, client_id: current_user.id)
+    service = Service.find(params[:service_id])
+    @rating = service.ratings.create(rating_params, client_id: current_user.id)
     unless @rating.save
       render json: {message: "400 Bad Request"}, status: :bad_request
     end
+    average_grade = Rating.where(service_id: params[:service_id]).average(:grade)
+    Service.update(params[:service_id],average_grade: average_grade)
+
   end
 
   private 
